461ada7cd8d8ec9d42b3e467aa9b023405ce2e12
yuhao-kuo <st40201@gmail.com>
2019-03-12 13:07:09 +0800

    add arduino example code

---
+++ arduino_code/Breathing_light/Breathing_light.ino
@@ -0,0 +1,23 @@
+int led;
+int fade;
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  led = 0;
+  fade = 5;
+}
+
+void loop()
+{
+  analogWrite(LED_BUILTIN, led);
+  led = led + fade;
+
+  if (led <= 0) {
+    fade = 5;
+  } else if (led >= 255) {
+    fade = -5;
+  }
+  delay(30);
+}
